5c623e57e420da0bd715c38f23b62b10012d1524
Travis K <tkupsche@gmail.com>
2024-11-16 14:29:45 -0800

    First layout

--- keyboards/crkbd/crkbd.c
+++ keyboards/crkbd/crkbd.c
@@ -49,10 +49,10 @@ static void oled_render_layer_state(void) {
             oled_write_ln_P(PSTR("Default"), false);
             break;
         case 1:
-            oled_write_ln_P(PSTR("Lower"), false);
+            oled_write_ln_P(PSTR("Paren-NumPad"), false);
             break;
         case 2:
-            oled_write_ln_P(PSTR("Raise"), false);
+            oled_write_ln_P(PSTR("Symbol-Arrows"), false);
             break;
         case 3:
             oled_write_ln_P(PSTR("Adjust"), false);
